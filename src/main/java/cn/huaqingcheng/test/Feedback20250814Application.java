de6dfdda358436815e548c02b527aaab7dd2cdac
冰凝 <liuli.dnn@email.cn>
2025-08-14 14:45:05 +0800

    ...

---
+++ src/main/java/cn/huaqingcheng/test/Feedback20250814Application.java
@@ -0,0 +1,13 @@
+package cn.huaqingcheng.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feedback20250814Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Feedback20250814Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/huaqingcheng/test/Feedback20250814ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.huaqingcheng.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feedback20250814ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
